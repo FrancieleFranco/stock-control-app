0d7082e5705d1589d18f689c2a1f2055718e05cc
franciele Franco <franciele.alves1108@gmail.com>
2023-12-27 09:47:11 -0300

    add relative form

--- stock-control/src/app/modules/home/home.component.ts
+++ stock-control/src/app/modules/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
   loginCard = true;
+
+  loginForm = this.formBuilder.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+
+  sigupForm = this.formBuilder.group({
+    name: ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  onSubmitLoginForm(): void {
+    console.log('Dados do formulário de login', this.loginForm.value);
+  }
+  onSubmitsigupForm(): void {
+    console.log('Dados do formulário de criação', this.sigupForm.value);
+  }
 }
