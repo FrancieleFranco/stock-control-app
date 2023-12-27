05f04eb28747160c01e41fbc663ba7867bc31f3d
franciele Franco <franciele.alves1108@gmail.com>
2023-12-27 15:10:24 -0300

    Creating module, component and route dashboard functionality

--- stock-control/src/app/app-routing.module.ts
+++ stock-control/src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './modules/home/home.component';
+import { DashboardHomeComponent } from './modules/dashboard/page/dashboard-home/dashboard-home.component';
 
 const routes: Routes = [
   {
     path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
     component: HomeComponent,
+  },
+  {
+    path: 'dashboard',
+    component: DashboardHomeComponent,
   },
 ];
 
---
+++ stock-control/src/app/modules/dashboard/page/dashboard-home/dashboard-home.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-home',
+  templateUrl: './dashboard-home.component.html',
+  styleUrls: [],
+})
+export class DashboardHomeComponent {}
