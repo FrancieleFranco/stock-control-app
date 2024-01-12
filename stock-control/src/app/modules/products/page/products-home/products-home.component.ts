3207eb4697f31a564177019f72f022cb411c2b99
franciele Franco <franciele.alves1108@gmail.com>
2024-01-12 16:28:33 -0300

    Generated module, route and product screen

---
+++ stock-control/src/app/modules/products/page/products-home/products-home.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-products-home',
+  templateUrl: './products-home.component.html',
+  styleUrls: [],
+})
+export class ProductsHomeComponent {}
---
+++ stock-control/src/app/modules/products/products.module.ts
@@ -0,0 +1,48 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { HttpClientModule } from '@angular/common/http';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { InputMaskModule } from 'primeng/inputmask';
+import { InputSwitchModule } from 'primeng/inputswitch';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { DialogService, DynamicDialogModule } from 'primeng/dynamicdialog';
+import { DropdownModule } from 'primeng/dropdown';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { TooltipModule } from 'primeng/tooltip';
+import { ConfirmationService } from 'primeng/api';
+import { ProductsHomeComponent } from './page/products-home/products-home.component';
+
+import { PRODUCTS_ROUTES } from './products.routing';
+
+@NgModule({
+  declarations: [ProductsHomeComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    SharedModule,
+    RouterModule.forChild(PRODUCTS_ROUTES),
+    HttpClientModule,
+    CardModule,
+    ButtonModule,
+    TableModule,
+    InputMaskModule,
+    InputSwitchModule,
+    InputTextModule,
+    InputTextareaModule,
+    InputNumberModule,
+    DynamicDialogModule,
+    DropdownModule,
+    ConfirmDialogModule,
+    TooltipModule,
+  ],
+  providers: [DialogService, ConfirmationService],
+})
+export class ProductsModule {}
